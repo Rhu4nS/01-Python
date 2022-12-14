39e54328ec56836d46e09273b4ee5430f9829ecd
Rhuan Santos <119426491+Rhu4nS@users.noreply.github.com>
2022-12-14 10:06:42 -0300

    Add files via upload

---
+++ desafio21.py
@@ -0,0 +1,10 @@
+# faça um programa em python que reproduza o audio de um arquivo em MP3 ***** abre o cmd e digita pip install pygame --pre ******
+
+import os, time
+'''r = caminho absoluto'''
+os.startfile(r'C:\Users\rhuan.santos\PycharmProjects\ambiente_de_aulaspy\Desafios_iniciante\ex021')
+'''time=tempo e  sleep=dormir parar a execução'''
+time.sleep(10)
+
+
+
