46d3b5f3091760e66ac00c09cbc8f2ca7d11f245
Rhuan Santos <119426491+Rhu4nS@users.noreply.github.com>
2022-12-16 14:16:21 -0300

    Update and rename 05-Sucessor e Antecessor to 05-Sucessor e Antecessor.py

--- 05-Sucessor e Antecessor.py
+++ 05-Sucessor e Antecessor.py
@@ -1,13 +1,13 @@
 # fazer um programa que leia um numero inteiro e mostra na tela o seu sucessor e antecessor:
-'''
+
 n1 = int(input('Digite um numero: '))
 n2 = n1 + 1
 n3 = n1 - 1
 
 print('O numero é:\n {} \n Seu sucessor:\n {} \n Seu antecessor:\n {}'.format(n1, n2, n3))
 
-'''
-#outro jeito de escrever
+
+# outro jeito de escrever
 
 n1 = int(input('Digite um numero: '))
 
