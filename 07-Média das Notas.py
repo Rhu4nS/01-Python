c0e77930fabd2353ea37aafaa286337c76ef9630
Rhuan Santos <119426491+Rhu4nS@users.noreply.github.com>
2022-12-16 14:17:11 -0300

    Update and rename 07-Média das Notas to 07-Média das Notas.py

--- 07-Média das Notas.py
+++ 07-Média das Notas.py
@@ -7,11 +7,11 @@ m= (n1+n2)/2
 print(' Sua primeira nota foi: {:.1f}\n Sua segunda nota foi: {:.1f}\n Sua média final foi: {:.1f}'.format(n1, n2, m))
 
 #outro jeito de escrever:
-'''
+
 n1 = float(input('Digite a primeira nota:'))
 n2 = float(input('Digite a segunda nota: '))
 print(' Sua primeira nota foi: {}\n Sua segunda nota foi: {}\n Sua média final foi: {}'.format(n1, n2, ((n1+n2)/2)))
-'''
+
 
 if m >= 6:
     print('parabéns, você está aprovado!')
