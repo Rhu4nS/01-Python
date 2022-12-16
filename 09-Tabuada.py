d4e3839095cd4c9565df46ff53827a3ec3ef1a9d
Rhuan Santos <119426491+Rhu4nS@users.noreply.github.com>
2022-12-16 14:18:15 -0300

    Update and rename 09-Tabuada to 09-Tabuada.py

--- 09-Tabuada.py
+++ 09-Tabuada.py
@@ -1,5 +1,5 @@
 # Faça um programa que leia um numero inteiro qualquer e mostre a sua tabuada
-'''
+
 n = int(input(' Digite um numero: '))
 
 n1 = n*1
@@ -14,7 +14,7 @@ n9 = n*9
 n10 = n*10
 
 print(' A tabuada do numero escolhido é:\n {}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}'.format(n1, n2, n3, n4, n5, n6, n7, n8, n9, n10))
-'''
+
 
 n = int(input(' Digite um numero para ver sua tabuada: '))
 print('¨'*20)
