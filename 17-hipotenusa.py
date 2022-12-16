9af3437e295f1139474bf3da8d6b233bd7bab800
Rhuan Santos <119426491+Rhu4nS@users.noreply.github.com>
2022-12-16 14:21:59 -0300

    Update 17-hipotenusa.py

--- 17-hipotenusa.py
+++ 17-hipotenusa.py
@@ -1,21 +1,21 @@
 #Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triangulo retangulo, calcule e mostre o comprimento da hipotenusa
 
 import math
-'''
+
 c1 = float(input(' Digite o valor do cateto adjacente:'))
 c2 = float(input(' Digite o valor do cateto oposto :'))
 h = math.sqrt(c1*c1+c2*c2)
 r = h ** 1/2
 print('Cateto adjacente: {}\nCateto oposto: {}\n Hipotenusa = {:.2f}'.format(c1, c2, h, r))
-'''
+
 
 # OUTRAS FORMAS DE ESCREVER
-'''
+
 c1 = float(input(' Digite o valor do cateto adjacente:'))
 c2 = float(input(' Digite o valor do cateto oposto :'))
 h = math.hypot(c1, c2)
 print('Cateto adjacente: {}\nCateto oposto: {}\n Hipotenusa = {:.2f}'.format(c1, c2, h))
-'''
+
 c1 = float(input(' Digite o valor do cateto adjacente:'))
 c2 = float(input(' Digite o valor do cateto oposto :'))
 
