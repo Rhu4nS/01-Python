4009c54f3c30ac42c58748c2cc1186b5e5b94cc9
Rhuan Santos <119426491+Rhu4nS@users.noreply.github.com>
2022-12-22 18:04:46 -0300

    Update and rename ex022.py to 22-Analisador de texto.py

--- 22-Analisador de texto.py
+++ 22-Analisador de texto.py
@@ -1,8 +1,10 @@
 '''
+
 Crie um programa que leia o nome completo de uma pessoa e mostre:
 - O nome com todas as letras maiusculas e minusculas
 - Quantas letras ao todo (sem contar os espaços)
 - Quantas letras tem o primeiro nome
+
 '''
 
 
